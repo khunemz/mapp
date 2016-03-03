14f905d20e2398aa42207e1a680d0a599117f4e4
khunemz <khunemz@gmail.com>
2016-03-04 00:05:31 +0700

    Install node_module to run gulp with laravel-elixir , run migration to create users and prducts databases.

---
+++ database/migrations/2016_03_03_163249_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('productTitle');
+            $table->string('productCaption');
+            $table->string('price');
+            $table->integer('image_id')->nullable();
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
