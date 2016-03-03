cdba55848760fee5f93c33350630904012ce9d93
khunemz <khunemz@gmail.com>
2016-03-04 01:02:11 +0700

    migrate add column boolean isAdmin to users table

---
+++ database/migrations/2016_03_03_175827_add_column_isAdmin_in_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnIsAdminInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->boolean('isAdmin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('isAdmin');
+    }
+}
