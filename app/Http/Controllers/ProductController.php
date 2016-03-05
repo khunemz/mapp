7664cfb30e8642d71246185a690b64e0419f7e8c
khunemz <khunemz@gmail.com>
2016-03-05 12:30:42 +0700

    Refactor store action

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -53,9 +53,22 @@ class ProductController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * 1. validate input data
+     * 2. Store input image to a variable call $file
+     * 3. Get original client name to name storing image
+     * 4. Move image to given File path with original name
+     * 5. Instantiate imageObje
+     * 6. In order to store file path on 'image' column
+     * 7. Instantiate Product object
+     * 8. In order to store product input in it
+     * 9. Save $product first , it will return $product
+     *  , including id
+     * 10. Create image which matches to returned id
+     *  from product , which saved before.
+     * 11. redirect
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Response $product
      */
     public function store(Request $request)
     {
@@ -68,30 +81,35 @@ class ProductController extends Controller
             'image' => 'mimes:jpeg,png'
         ]);
 
-        //Store $request to database
-        $product = new Product();
-        //store request image file on $file
+
+        //Store request image file on $file
         $file = $request->file('image');
-        //get image file name
+        //get image file name (original)
         $filePath = $file->getClientOriginalName();
+        //Move file to specified folder with original name
         $file->move( public_path() . '/uploads/', $filePath);
+        //instantiate Image (has-many) object
+        $imageObj = new \App\Image();
+        //Assign $filePath to $imageObj
+        $imageObj->image = $filePath;
 
         //Match table column to data input name
+        $product = new Product();
         $product->productTitle = $request->productTitle;
         $product->productCaption = $request->productCaption;
         $product->price = $request->price;
         $product->category = $request->category;
-        $product->images()->image = $file->getRealPath();
-        //move file to public/uploads
+        //Store to database
+        $product->save();
+        $product->images()->create([
+            'image' => $imageObj->image]);
 
 
-        //Check success and redirect
-        if($product->save()){
         return redirect()->route('product.index')->with([
             'product'=> $product
             ]);
-        }
     }
+
 
     /**
      * Show data with ID.
